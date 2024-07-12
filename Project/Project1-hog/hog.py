e8da61b12474d3c77a77beddeac33f8a0190317d
kekeandzeyu <kekeandzeyu@gmail.com>
2024-07-12 15:50:40 +0800

    Finish up project 1 - hog!

--- Project/Project1-hog/hog.py
+++ Project/Project1-hog/hog.py
@@ -295,8 +295,8 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
 
 def sus_strategy(score, opponent_score, threshold=11, num_rolls=6):
     """This strategy returns 0 dice when your score would increase by at least threshold."""
-    if sus_update(num_rolls, score, opponent_score) - score >= threshold:
-        return sus_update(num_rolls, score, opponent_score)
+    if sus_update(0, score, opponent_score) - score >= threshold:
+        return 0
     else:
         return num_rolls
 
