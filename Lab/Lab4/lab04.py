bd8ece1dd150bacfe3c46e5dbe5936d56593a6d6
kekeandzeyu <kekeandzeyu@Keke-Dell>
2024-07-22 16:00:13 +0800

    Finish up lab 4!

---
+++ Lab/Lab4/lab04.py
@@ -0,0 +1,223 @@
+LAB_SOURCE_FILE = __file__
+
+
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])
+    [1, 2, 3]
+    >>> deep = [1, [[2], 3], 4, [5, 6]]
+    >>> flatten(deep)
+    [1, 2, 3, 4, 5, 6]
+    >>> deep                                # input list is unchanged
+    [1, [[2], 3], 4, [5, 6]]
+    >>> very_deep = [['m', ['i', ['n', ['m', 'e', ['w', 't', ['a'], 't', 'i', 'o'], 'n']], 's']]]
+    >>> flatten(very_deep)
+    ['m', 'i', 'n', 'm', 'e', 'w', 't', 'a', 't', 'i', 'o', 'n', 's']
+    """
+    lst = []
+    for elem in s:
+        if type(elem) == list:
+            lst += flatten(elem)
+        else:
+            lst += [elem]
+    return lst
+
+
+# Alternate solution
+#     if not s:
+#         return []
+#     elif type(s[0]) == list:
+#         return flatten(s[0]) + flatten(s[1:])
+#     else:
+#         return [s[0]] + flatten(s[1:])
+
+
+def merge(s, t):
+    """Merges two sorted lists.
+
+    >>> s1 = [1, 3, 5]
+    >>> s2 = [2, 4, 6]
+    >>> merge(s1, s2)
+    [1, 2, 3, 4, 5, 6]
+    >>> s1
+    [1, 3, 5]
+    >>> s2
+    [2, 4, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    >>> merge([2, 3, 4], [2, 4, 6])
+    [2, 2, 3, 4, 4, 6]
+    >>> from construct_check import check
+    >>> check(LAB_SOURCE_FILE, 'merge', ['While', 'For'])    # ban iteration
+    True
+    """
+    # Recursive Solution
+    if not s or not t:
+        return s + t
+    elif s[0] < t[0]:
+        return [s[0]] + merge(s[1:], t)
+    else:
+        return [t[0]] + merge(s, t[1:])
+
+    # Alternative Recursive Solution
+    # if not s or not t:
+    #     return s + t
+    # elif s[0] <= t[0]:
+    #     return [s[0]] + merge(s[1:], t)
+    # else:
+    #     return merge(t, s)
+    # For the last line, instead of concatenating [t[0]] at the front like we did for the first solution,
+    # we instead call merge again with the arguments switched, so that in the next call to merge, s
+    # is now t, and t is s.
+    # Note that in order for this to work successfully, we have to modify the elif case to have <= rather
+    # than just <, otherwise we run into infinite recursion. (Try out the last doctest to see why)
+
+
+def size_of_tree(t):
+    """Return the number of entries in the tree.
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    >>> size_of_tree(numbers)
+    7
+    """
+    return 1 + sum([size_of_tree(t) for t in branches(t)])
+
+
+# Alternate solution
+def size_of_tree(t):
+    subtrees_sum = 0
+    for subtree in branches(t):
+        subtrees_sum += size_of_tree(subtree)
+    return 1 + subtrees_sum
+
+
+def replace_loki_at_leaf(t, lokis_replacement):
+    """Returns a new tree where every leaf value equal to "loki" has
+    been replaced with lokis_replacement.
+
+    >>> yggdrasil = tree('odin',
+    ...                  [tree('balder',
+    ...                        [tree('loki'),
+    ...                         tree('freya')]),
+    ...                   tree('frigg',
+    ...                        [tree('loki')]),
+    ...                   tree('loki',
+    ...                        [tree('sif'),
+    ...                         tree('loki')]),
+    ...                   tree('loki')])
+    >>> laerad = copy_tree(yggdrasil)    # copy yggdrasil for testing purposes
+    >>> print_tree(replace_loki_at_leaf(yggdrasil, 'freya'))
+    odin
+      balder
+        freya
+        freya
+      frigg
+        freya
+      loki
+        sif
+        freya
+      freya
+    >>> laerad == yggdrasil    # Make sure original tree is unmodified
+    True
+    """
+    if is_leaf(t) and label(t) == "loki":
+        return tree(lokis_replacement)
+    else:
+        bs = [replace_loki_at_leaf(b, lokis_replacement) for b in branches(t)]
+        return tree(label(t), bs)
+
+
+def divide(quotients, divisors):
+    """Return a dictonary in which each quotient q is a key for the list of
+    divisors that it divides evenly.
+
+    >>> divide([3, 4, 5], [8, 9, 10, 11, 12])
+    {3: [9, 12], 4: [8, 12], 5: [10]}
+    >>> divide(range(1, 5), range(20, 25))
+    {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
+    """
+    return {q: [d for d in divisors if d % q == 0] for q in quotients}
+
+
+# Tree Data Abstraction
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
+
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
